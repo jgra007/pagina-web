c129ce879ded9aee8f8df779997c845f7065aa52
jgra007 <66275651+jgra007@users.noreply.github.com>
2021-09-21 07:16:45 -0500

    Delete 9-prime.js

--- 9-prime.js
+++
@@ -1,7 +0,0 @@
-function countPrimeNumbers() {
-    return 25;
-  }
-  const init = performance.now()
-  countPrimeNumbers();
-  const end = performance.now()
-  console.log(`Execution time of printing countPrimeNumbers was ${end - init} milliseconds.`);
